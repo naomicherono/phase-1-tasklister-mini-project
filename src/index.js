bb9e40db0fcba29a507278e9d3f7fc5d4130c269
naomicherono <nonniecheronoh12@gmail.com>
2023-06-29 12:54:38 +0300

    updated my js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    taskDescription(e.target['new-task-description'].value)
+    form.reset();
+  })
+  function taskDescription(task){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click',deleteElement)
+    btn.textContent = 'x'
+    p.textContent = `${task}`
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector('#list').appendChild(p)
+  };
+  function deleteElement(e){
+    e.target.parentNode.remove()
+  }
 });
